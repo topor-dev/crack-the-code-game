868355741efb62e590e727409ca0492216e7c402
Vadim Tokarev <16782067+topor-dev@users.noreply.github.com>
2019-02-07 15:23:26 +0300

    Create interface

--- crack_the_code_game/static/js/main.js
+++ crack_the_code_game/static/js/main.js
@@ -22,10 +22,27 @@ const ValidationResult = {
     ColorMatch: 1,
 }
 
+const GameStageEnum = {
+	INIT: 0,
+	GAME: 1,
+	AFTER_GAME: 2,
+}
+
+
+Vue.component('rules', {
+	template:`<div>
+		<hr>
+		<p>Правила:</p>
+		<p>Нужно подобрать правильную последовательность цифр.</p>
+		<p>В случае, если цифра верная и стоит на правильной позиции - белый круг</p>
+		<p>В случае, если цифра верная, но стоит на неправильной позиции - черный круг</p>
+		<hr>
+	</div>`
+})
 
 Vue.component('new_game_button',{
 	methods: {
-		new_game: function(){
+		new_game(){
 			param = '?' +
 					'cell_count=' + this.$data.cell_count + '&' +
 					'max_color=' + this.$data.max_color
@@ -40,7 +57,7 @@ Vue.component('new_game_button',{
 				.catch((error)=>console.log(error))
 		}
 	},
-	data: function(){
+	data(){
 		return {
 			cell_count: 3,
 			max_color: 4,
@@ -53,70 +70,120 @@ Vue.component('new_game_button',{
 })
 
 
-Vue.component('tester', {
-	methods:{
-		go_crack: function(){
-			var key = []
-			for(var elem of this.$el.getElementsByTagName('select')){
-				key.push(elem.selectedIndex)
-			}
-			this.$root.crack(key)
-		},
+Vue.component('variant', {
+	props: {
+		init_id: {},
+		init_color: {default: 'white'},
 	},
-	props: ['selects_count', 'max_color_count'],	
-	template: '<div><select v-for="select_id in selects_count" :key="select_id" :id="select_id-1">' +
-			  '<option v-for="item in (max_color_count+1)" :key="item">{{item-1}}</option>' +
-			  '</select>' +
-			  '<button v-if="selects_count > 0" v-on:click="go_crack">попробовать этот вариант</button></div>',
+	data(){
+		return {
+			class_: '',
+			id: this.init_id,
+			color: this.init_color,
+		}
+	},
+	template: `<div class="variant"
+		:style="{
+			'background-color': color
+		}"
+		:class="class_"
+	></div>`,
 })
 
-Vue.component('crack_result', {
-	props: ['res'],
-	computed: {
-		res2class: function(){
-			return this.res.crack_result.map(e=>{
-				switch(e){
-					case ValidationResult.ColorAndPlaceMatch:
-						return 'full-match'
-					case ValidationResult.ColorMatch:
-						return 'partial-match'
+
+Vue.component('variants', {
+	props:['colors'],
+	methods: {
+		make_variant_active(e, id) {
+			for(let child of this.$children){
+				if (child.$data.id == id){
+					child.$data.class_ = 'active'
+					continue
 				}
-				return ''
-			})
+				child.class_ = ''
+			}
 		},
 	},
-	template: '<div><span v-text="res.state"></span> - <div v-for="(item,index) in res2class" :key="index" class="crack-result" :class="item"></div></div>',
+	template: `<div class="variants">
+	<variant
+		v-for="(color,id) in colors"
+		:key="id"
+		@click.native="make_variant_active($event, id)"
+		:init_color="color"
+		:init_id="id"
+	/>
+	</div>`,
 })
 
 
-new Vue({
-	el: '#main', // {{game}}
-	template: '<div><new_game_button></new_game_button><br/>' +
-			  '<div v-if="game.attempts_remind > -1">попыток осталось: {{game.attempts_remind}}</div>' +
-			  '<tester style="display:inline" :selects_count=selects_count :max_color_count=max_color_count></tester>' +
-			  '<div v-if="game.attempts && game.attempts.length > 0">' +
-			  	'<div>ключ - результат</div>' +
-				'<crack_result v-for="(res,index) in game.attempts" :key="index" :res=res></crack_result>' +
-			  '</div>' + 
-			  '</div>',
+Vue.component('attempt', {
+	props: {
+		count: {default: 5},
+		result: {default: ()=>[]}, // should be factory
+	},
+	computed: {
+	},
+	template: `<div class="attempt">
+		<div>
+			<variant v-for="id in count" :key="id" :init_id="id"/>
+		</div>
+		<div v-if="result.length > 0">
+			<div><hr/></div>
+			<variant v-for="e in result" :init_color="e && 'white' || 'black'"/>
+		</div>
+	</div>`
+})
+
+
+Vue.component('detect-button', {
+	template: `<div id="detect-button">
+		проверить
+	</div>`
+})
+
+Vue.component('game', {
+	props: ['colors', 'attempts'],
+	template: `<div id="game-container">
+		<hr/>
+		<variants :colors="colors"/>
+		<hr/>
+		<div id="attempts-container">
+			<attempt/>
+			<detect-button/>
+			<div id="attempts-history-container">
+				<attempt v-for="att in attempts" :key="att" :result="[1,0,0]"/>
+			</div>
+		</div>
+	</div>`,
+})
+
+
+const app = new Vue({
+	el: '#main',
 	data: {
+		colors: ['red', 'green', 'blue', 'yellow', 'violet', 'cyan','white'],
 		game: {},
 		id: -1,
+		game_stage: GameStageEnum.INIT,
 	},
 	computed: {
-		selects_count: function(){
+		selects_count(){
 			return this.$data.game.cell_count || 0
 		},
-		max_color_count: function(){
+		max_color_count(){
 			return this.$data.game.max_color || 0
+		},
+		game_stage_enum(){
+			return GameStageEnum
 		},
 	},
 	methods: {
-		new_game: function(data){
+		new_game(data){
 			this.$root.$data.game = data
 			this.$root.$data.id = data.id
+			this.$root.$data.game_stage = GameStageEnum.GAME
 		},
-		crack: function(state){
+		crack(state){
 			that = this
 			var str_state = state.join(',')
 			var settings = API.validate
@@ -144,5 +211,10 @@ new Vue({
 				})
 				.catch((error)=>console.log(error))
 		}
-	}
+	},
+	template: `<div>
+	<rules v-if="game_stage == game_stage_enum.INIT"/>
+	<new_game_button v-if="game_stage != game_stage_enum.GAME"/>
+	<game :colors="colors" :attempts="20" v-if="game_stage == game_stage_enum.GAME"/>
+	</div>`,
 })
